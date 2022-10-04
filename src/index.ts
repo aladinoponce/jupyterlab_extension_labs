85c2b695a59d622ae079886947a88a710997f727
Aladino Ponce Araya <aladino.ponce@mercadolibre.cl>
2022-10-04 14:20:12 -0300

    add pictures

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils'
+import { Widget } from '@lumino/widgets';
 
 /**
  * Initialization data for the test_labs extension.
@@ -9,8 +11,31 @@ import {
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'test_labs:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension test_labs is activated!');
+    const content = new Widget();
+    const widget = new MainAreaWidget({ content })
+    widget.id = 'test_labs';
+    widget.title.label = 'Meli Labs';
+    widget.title.closable = true;
+
+    const command: string = 'test_labs:open';
+
+    app.commands.addCommand(command, {
+      label: 'Random Astronomy Picture',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Labs Develop' });
   }
 };
 
